a6babd8ccfb614168fc060ab918eeecf2830ace6
Jibbe23 <jibbedehibbe@yahoo.de>
2019-06-12 09:13:27 +0200

    First Servlet project: class HelloWorldServlet

---
+++ servletdemo/src/com/jibbedehibbe/servletdemo/HelloWorldServlet.java
@@ -0,0 +1,58 @@
+package com.jibbedehibbe.servletdemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/HelloWorldServlet")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Step 1: set content type
+		response.setContentType("text/html");
+		
+		// Step 2: get the printwriter(import PrintWriter)
+		PrintWriter out = response.getWriter();
+		
+		// Step 3: generate HTML content
+		out.println("<html><body>");
+		
+		out.println("<h2>Hello World!</h2>");
+		out.println("<hr>");
+		out.println("Time on the Server is: " + new java.util.Date());
+	
+		
+		out.println("</body></html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
